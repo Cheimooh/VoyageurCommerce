e08ee71c6ec43849bc604b4b690fdf75e41fff42
Cheimooh <maeva.somny@edu.univ-fcomte.fr>
2019-03-19 17:24:10 +0100

    djikstra <3

--- Graph.java
+++ Graph.java
@@ -22,32 +22,77 @@ public class Graph {
 
     public void initMatriceDistance(){
         matriceDistance = new int[noeuds.size()][noeuds.size()];
+
+        List<Node> nonMarques = new ArrayList<>();
+        List<Integer> poidsNonMarque = new ArrayList<>();
         for (int i = 0; i < noeuds.size(); i++) {
-            for (int j = 0; j < noeuds.size(); j++) {
-                matriceDistance[i][j] = -1;
-                if(i == j) matriceDistance[i][j] = 0;
-            }
+            nonMarques.add(noeuds.get(i));
+            poidsNonMarque.add(-1);
         }
-        for (int i = 0; i < noeuds.size(); i++) {
-            Node n1 = noeuds.get(i);
+
+        int[] lDistance = new int[noeuds.size()];
+
             for (int j = 0; j < noeuds.size(); j++) {
-                Node n2 = noeuds.get(j);
-                if (n1.getBrother().contains(n2)){
-                    for (int k = 0; k < arretes.size(); k++) {
-                        if (arretes.get(k).getNoeudDepart() == n1 && arretes.get(k).getNoeudArrivee() == n2
-                                || arretes.get(k).getNoeudDepart() == n2 && arretes.get(k).getNoeudArrivee() == n1 ){
-                            matriceDistance[i][j] = arretes.get(k).getDistance();
+                lDistance[j] = -1;
+            }
+            matriceDistance[0] = parcoursDesVoisins(noeuds.get(0), nonMarques, lDistance, 0);
+
+//        for (int i = 0; i < noeuds.size(); i++) {
+//            System.out.println();
+//            for (int j = 0; j < noeuds.size(); j++) {
+//                System.out.print(matriceDistance[i][j] + " ");
+//            }
+//        }
+    }
+
+    public int[] parcoursDesVoisins(Node noeudDepart, List<Node> nonMarques, int[] lDistance, int indiceOriginel) {
+        nonMarques.remove(noeudDepart);
+
+        //ajoute les distance avec des voisins
+        for (int i = 0; i < noeudDepart.getBrother().size(); i++) {
+            lDistance[indiceOriginel] = 0;
+            Node n = noeudDepart.getBrother().get(i);
+            int distance = -1;
+            for (int j = 0; j < arretes.size(); j++) {
+                if (arretes.get(j).getNoeudDepart() == noeudDepart && arretes.get(j).getNoeudArrivee() == n ||
+                        arretes.get(j).getNoeudDepart() == n && arretes.get(j).getNoeudArrivee() == noeudDepart){
+                    distance = arretes.get(j).getDistance();
+
+                    if (lDistance[noeuds.indexOf(n)] == -1 && nonMarques.contains(n)) {
+                        lDistance[noeuds.indexOf(n)] = lDistance[noeuds.indexOf(noeudDepart)] + distance;
+                    }
+                    else if (lDistance[noeuds.indexOf(n)] > 0 && nonMarques.contains(n)){
+                        if (lDistance[noeuds.indexOf(noeudDepart)] + distance < lDistance[noeuds.indexOf(n)]) {
+                            lDistance[noeuds.indexOf(n)] = lDistance[noeuds.indexOf(noeudDepart)] + distance;
                         }
                     }
                 }
             }
         }
-        for (int i = 0; i < noeuds.size(); i++) {
+         //si tout les noeuds sont marqués
+        if (nonMarques.isEmpty()){
+            for (int i = 0; i < lDistance.length; i++) {
+                System.out.print(lDistance[i] + " ");
+            }
             System.out.println();
-            for (int j = 0; j < noeuds.size(); j++) {
-                System.out.print(matriceDistance[i][j] + " ");
+            return lDistance;
+        }
+
+        //si il reste des noeuds non marqués
+        noeudDepart = nonMarques.get(0);
+        int poids = lDistance[noeuds.indexOf(nonMarques.get(0))];
+        for (int i = 0; i < nonMarques.size(); i++) {
+            int tempPoids = lDistance[noeuds.indexOf(nonMarques.get(i))];
+            if (poids == -1){
+                poids = tempPoids;
+                noeudDepart = nonMarques.get(i);
+            }
+            if (tempPoids<poids && tempPoids > 0) {
+                poids = tempPoids;
+                noeudDepart = nonMarques.get(i);
             }
         }
+        return parcoursDesVoisins(noeudDepart, nonMarques, lDistance, indiceOriginel);
     }
 
     public int addArrete(Node noeud1, Node noeud2, int distance) {
@@ -78,9 +123,6 @@ public class Graph {
         }
         return noeudsVisites;
     }
-
-
-
 
     public List<Node> getNoeuds() {
         return noeuds;
